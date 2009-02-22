1917ac3e8108502f37b555aeacdb6b6cb7a09e27
Stefan Plantikow <stepn@noname.(none)>
2009-02-22 20:24:07 +0100

    selfActor factored out; added deferred sending

---
+++ src/main/scala/de/jasminelli/sofleuse/util/DeferredSending.scala
@@ -0,0 +1,10 @@
+package de.jasminelli.sofleuse.util
+/**
+ * Created by IntelliJ IDEA.
+ * User: stepn
+ * Date: 22.02.2009
+ * Time: 18:45:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait DeferredSending 
